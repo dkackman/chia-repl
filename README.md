c501731ab0411afcab09aff5697ca113f77e7a47
Don Kackman <dkackman@gmail.com>
2022-04-05 22:18:45 -0500

    rough sketch

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # chia-repl
+
 A REPL tool for Chia
+
+## install
+
+```shell
+cd src
+npm install
+```
+
+## run
+
+```shell
+cd src
+npm start
+.help
+```
+
+## example
+
+The global `options` context object has the host, port and path to cert files. These properties can be set inside the repl with `options.host = "my-host"`
+
+```shell
+PS C:\Users\dkack\src\github\dkackman\chia-repl\src> npm start
+Debugger attached.
+
+> ws-client@1.0.0 start C:\Users\dkack\src\github\dkackman\chia-repl\src
+> node index.js
+
+Debugger attached.
+> .help
+.break        Sometimes you get stuck, this gets you out
+.clear        Break, and also clear the local context
+.connect      Opens the websocket connection to the chia daemon
+.daemon       Sends a command to the chia daemon
+.disconnect   Closes the websocket connection to the chia daemon
+.editor       Enter editor mode
+.exit         Exit the REPL
+.help         Print this help message
+.load         Load JS from a file into the REPL session
+.save         Save all evaluated commands in this REPL session to a file
+
+Press Ctrl+C to abort current expression, Ctrl+D to exit the REPL
+> options
+{
+  host: 'localhost',
+  port: 55400,
+  key_path: '~/.chia/mainnet/config/ssl/daemon/private_daemon.key',
+  cert_path: '~/.chia/mainnet/config/ssl/daemon/private_daemon.crt'
+}
+> .connect
+Connecting...
+{
+  "ack": true,
+  "command": "register_service",
+  "data": {
+    "success": true
+  },
+  "destination": "chia_repl",
+  "origin": "daemon",
+  "request_id": "293579bcedd5ab377bbd9a514fcd2012af6d2b2c4c17ee6f563018cfb43b3be9"
+}
+> .daemon is_keyring_locked
+{
+  "ack": true,
+  "command": "is_keyring_locked",
+  "data": {
+    "is_keyring_locked": false,
+    "success": true
+  },
+  "destination": "chia_repl",
+  "origin": "daemon",
+  "request_id": "bc797093c3cf89d2456e3b3e3f68f8c9f2ead4c85e9aca53624aa3f3c6423154"
+}
+> .disconnect
+Disconnected...
+>
+```
--- src/index.js
+++ src/index.js
@@ -2,26 +2,84 @@ const wslib = require('ws');
 const fs = require('fs');
 const cb = require('crypto-browserify');
 const repl = require('repl');
+const os = require("os");
 
 const options = {
-    rejectUnauthorized: false,
-    key: fs.readFileSync('C:\\Users\\dkack\\.rchia\\certs\\chiapas\\private_daemon.key'),
-    cert: fs.readFileSync('C:\\Users\\dkack\\.rchia\\certs\\chiapas\\private_daemon.crt')
-  };
-const ws = new wslib.WebSocket('wss://chiapas:55400', options);
-repl.start('> ').context.m = "m";
+  host: "chiapas",
+  port: 55400,
+  key_path: '~/.chia/mainnet/config/ssl/daemon/private_daemon.key',
+  cert_path: '~/.chia/mainnet/config/ssl/daemon/private_daemon.crt'
+};
 
-ws.on('open', function open() {
-  const msg = formatMessage("register_service", "daemon", { service: "chia_repl" });
-  ws.send(msg);
+const replServer = repl.start({ prompt: '> ', useColors: true });
+replServer.on('exit', () => {
+  if (replServer.context.ws !== undefined) {
+    replServer.context.ws.close();
+    replServer.context.ws = undefined;
+  }
+  process.exit();
 });
 
-ws.on('message', function message(data) {
-  console.log(JSON.stringify(JSON.parse(data), null, 2));
+replServer.context.options = options;
+replServer.defineCommand('connect', {
+  help: 'Opens the websocket connection to the chia daemon',
+  action() {
+    if (replServer.context.ws !== undefined) {
+      console.log("already connected. Use .disconnect first");
+      return;
+    }
+
+    const options = {
+      rejectUnauthorized: false,
+      key: fs.readFileSync(replServer.context.options.key_path.replace("~", os.homedir())),
+      cert: fs.readFileSync(replServer.context.options.cert_path.replace("~", os.homedir())),
+    };
+    const ws = new wslib.WebSocket(`wss://${replServer.context.options.host}:${replServer.context.options.port}`, options);
+    replServer.context.ws = ws;
+
+    ws.on('open', function open() {
+      console.log("Connecting...");
+      const msg = formatMessage("register_service", "daemon", { service: "chia_repl" });
+      ws.send(msg);
+    });
+
+    ws.on('message', function message(data) {
+      console.log(JSON.stringify(JSON.parse(data), null, 2));
+      replServer.displayPrompt();
+    });
+
+    ws.on('close', function message(data) {
+      console.log("Disconnected...");
+      replServer.displayPrompt();
+    });
+  }
 });
 
-function formatMessage(command, destination, data = {})
-{
+replServer.defineCommand('disconnect', {
+  help: 'Closes the websocket connection to the chia daemon',
+  action() {
+    if (replServer.context.ws !== undefined) {
+      replServer.context.ws.close();
+      replServer.context.ws = undefined;
+    } else {
+      console.log("not connected");
+    }
+  }
+});
+
+replServer.defineCommand('daemon', {
+  help: 'Sends a command to the chia daemon',
+  action(command) {
+    if (replServer.context.ws !== undefined) {
+      const msg = formatMessage(command, "daemon");
+      replServer.context.ws.send(msg);
+    } else {
+      console.log("not connected");
+    }
+  }
+});
+
+function formatMessage(command, destination, data = {}) {
   var message = {};
   message.command = command;
   message.origin = "chia_repl";
