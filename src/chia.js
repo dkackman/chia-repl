634f069c55b89b1cde70f44031ae502a16d03828
Don Kackman <dkackman@gmail.com>
2022-04-07 00:41:01 -0500

    eslint

--- src/chia.js
+++ src/chia.js
@@ -1,109 +1,108 @@
 import { randomBytes } from 'crypto-browserify';
 import { WebSocket } from 'ws';
 import { readFileSync } from 'fs';
-import { homedir } from "os";
+import { homedir } from 'os';
 
-'use strict';
 class Chia {
-  constructor(options) {
-    this.options = options;
-    this.outgoing = {}; // outgoing messages awaiting a response
-    this.incoming = {}; // incoming responses 
-  }
-
-  connect(callback) {
-    if (this.ws !== undefined) {
-      throw new Error("Already connected");
+    constructor(options) {
+        this.options = options;
+        this.outgoing = {}; // outgoing messages awaiting a response
+        this.incoming = {}; // incoming responses 
     }
 
-    const wsOptions = {
-      rejectUnauthorized: false,
-      key: readFileSync(this.options.key_path.replace("~", homedir())),
-      cert: readFileSync(this.options.cert_path.replace("~", homedir())),
-    };
-    const address = `wss://${this.options.host}:${this.options.port}`;
-    const ws = new WebSocket(address, wsOptions);
+    connect(callback) {
+        if (this.ws !== undefined) {
+            throw new Error('Already connected');
+        }
 
-    ws.on('open', () => {
-      console.log(`Connecting to ${address}...`);
-      const msg = formatMessage("daemon", "register_service", { service: "chia_repl" });
-      ws.send(JSON.stringify(msg));
-    });
+        const wsOptions = {
+            rejectUnauthorized: false,
+            key: readFileSync(this.options.key_path.replace('~', homedir())),
+            cert: readFileSync(this.options.cert_path.replace('~', homedir())),
+        };
+        const address = `wss://${this.options.host}:${this.options.port}`;
+        const ws = new WebSocket(address, wsOptions);
 
-    ws.on('message', (data) => {
-      const msg = JSON.parse(data);
+        ws.on('open', () => {
+            console.log(`Connecting to ${address}...`);
+            const msg = formatMessage('daemon', 'register_service', { service: 'chia_repl' });
+            ws.send(JSON.stringify(msg));
+        });
 
-      if (this.outgoing[msg.request_id] !== undefined) {
-        this.outgoing[msg.request_id] = undefined;
-        this.incoming[msg.request_id] = msg;
-      } else if (callback !== undefined && msg.command == "register_service") {
-        callback(); //a little bit hacky way to do a callback on first connection
-      }
-    });
+        ws.on('message', (data) => {
+            const msg = JSON.parse(data);
 
-    ws.on('error', (e) => {
-      console.log(e);
-    });
+            if (this.outgoing[msg.request_id] !== undefined) {
+                this.outgoing[msg.request_id] = undefined;
+                this.incoming[msg.request_id] = msg;
+            } else if (callback !== undefined && msg.command === 'register_service') {
+                callback(); //a little bit hacky way to do a callback on first connection
+            }
+        });
 
-    ws.on('close', () => {
-      console.log("Disconnecting...");
-      if (callback !== undefined) {
-        callback();
-      }
-    });
+        ws.on('error', (e) => {
+            console.log(e);
+        });
 
-    this.ws = ws;
-  }
+        ws.on('close', () => {
+            console.log('Disconnecting...');
+            if (callback !== undefined) {
+                callback();
+            }
+        });
 
-  disconnect() {
-    if (this.ws === undefined) {
-      throw new Error("Not connected");
+        this.ws = ws;
     }
 
-    this.ws.close();
-    this.ws = undefined;
-  }
+    disconnect() {
+        if (this.ws === undefined) {
+            throw new Error('Not connected');
+        }
 
-  async sendCommand(destination, command, data) {
-    if (this.ws === undefined) {
-      throw new Error("Not connected");
+        this.ws.close();
+        this.ws = undefined;
     }
 
-    const outgoingMsg = formatMessage(destination, command, data);
-    const id = outgoingMsg.request_id;
-    this.outgoing[id] = outgoingMsg;
+    async sendCommand(destination, command, data) {
+        if (this.ws === undefined) {
+            throw new Error('Not connected');
+        }
 
-    this.ws.send(JSON.stringify(outgoingMsg));
+        const outgoingMsg = formatMessage(destination, command, data);
+        const id = outgoingMsg.request_id;
+        this.outgoing[id] = outgoingMsg;
 
-    const timer = ms => new Promise(res => setTimeout(res, ms));
-    const start = Date.now();
-    while (this.incoming[id] === undefined) {
-      await timer(100);
-      const elapsed = Date.now() - start;
-      if (elapsed / 1000 > this.options.timeout_seconds) {
-        throw new Error("Timeout expired");
-      }
+        this.ws.send(JSON.stringify(outgoingMsg));
+
+        const timer = ms => new Promise(res => setTimeout(res, ms));
+        const start = Date.now();
+        while (this.incoming[id] === undefined) {
+            await timer(100);
+            const elapsed = Date.now() - start;
+            if (elapsed / 1000 > this.options.timeout_seconds) {
+                throw new Error('Timeout expired');
+            }
+        }
+
+        const incomingMsg = this.incoming[id];
+        this.incoming[id] = undefined;
+        const incomingData = incomingMsg.data;
+        if (incomingData.success === false) {
+            throw new Error(incomingData.error);
+        }
+        return incomingData;
     }
-
-    const incomingMsg = this.incoming[id];
-    this.incoming[id] = undefined;
-    const incomingData = incomingMsg.data;
-    if (incomingData.success === false) {
-      throw new Error(incomingData.error);
-    }
-    return incomingData;
-  }
 }
 
 function formatMessage(destination, command, data = {}) {
-  return {
-    command: command,
-    origin: "chia_repl",
-    destination: destination,
-    ack: false,
-    request_id: randomBytes(32).toString('hex'),
-    data: data,
-  };
+    return {
+        command: command,
+        origin: 'chia_repl',
+        destination: destination,
+        ack: false,
+        request_id: randomBytes(32).toString('hex'),
+        data: data,
+    };
 }
 
 const _Chia = Chia;
--- src/index.js
+++ src/index.js
@@ -1,63 +1,63 @@
 import { start } from 'repl';
-import { Chia } from "./chia.js";
+import { Chia } from './chia.js';
 
 const replServer = start({ prompt: '> ', useColors: true });
 replServer.context.options = {
-  host: "localhost",
-  port: 55400,
-  key_path: '~/.chia/mainnet/config/ssl/daemon/private_daemon.key',
-  cert_path: '~/.chia/mainnet/config/ssl/daemon/private_daemon.crt',
-  timeout_seconds: 30,
+    host: 'localhost',
+    port: 55400,
+    key_path: '~/.chia/mainnet/config/ssl/daemon/private_daemon.key',
+    cert_path: '~/.chia/mainnet/config/ssl/daemon/private_daemon.crt',
+    timeout_seconds: 30,
 };
 
 replServer.on('exit', () => {
-  if (replServer.context.chiaServer !== undefined) {
-    replServer.context.chiaServer.disconnect();
-    replServer.context.chiaServer = undefined;
-  }
-  process.exit();
+    if (replServer.context.chiaServer !== undefined) {
+        replServer.context.chiaServer.disconnect();
+        replServer.context.chiaServer = undefined;
+    }
+    process.exit();
 });
 
 replServer.defineCommand('connect', {
-  help: 'Opens the websocket connection to the chia daemon. Enables these awaitable functions: crawler, daemon, farmer, full_node, harvester, wallet',
-  action() {
-    if (replServer.context.chiaServer !== undefined) {
-      console.log("Already connected. Use .disconnect first");
-      replServer.displayPrompt();
+    help: 'Opens the websocket connection to the chia daemon. Enables these awaitable functions: crawler, daemon, farmer, full_node, harvester, wallet',
+    action() {
+        if (replServer.context.chiaServer !== undefined) {
+            console.log('Already connected. Use .disconnect first');
+            replServer.displayPrompt();
+        }
+        else {
+            const chiaServer = new Chia(replServer.context.options);
+            chiaServer.connect(() => {
+                console.log('done');
+                replServer.displayPrompt();
+            });
+            replServer.context.chiaServer = chiaServer;
+            replServer.context.daemon = async (command, data) => chiaServer.sendCommand('daemon', command, data);
+            replServer.context.full_node = async (command, data) => chiaServer.sendCommand('chia_full_node', command, data);
+            replServer.context.wallet = async (command, data) => chiaServer.sendCommand('chia_wallet', command, data);
+            replServer.context.farmer = async (command, data) => chiaServer.sendCommand('chia_farmer', command, data);
+            replServer.context.harvester = async (command, data) => chiaServer.sendCommand('chia_harvester', command, data);
+            replServer.context.crawler = async (command, data) => chiaServer.sendCommand('chia_crawler', command, data);
+        }
     }
-    else {
-      const chiaServer = new Chia(replServer.context.options);
-      chiaServer.connect(() => {
-        console.log("done");
-        replServer.displayPrompt();
-      });
-      replServer.context.chiaServer = chiaServer;
-      replServer.context.daemon = async (command, data) => chiaServer.sendCommand("daemon", command, data);
-      replServer.context.full_node = async (command, data) => chiaServer.sendCommand("chia_full_node", command, data);
-      replServer.context.wallet = async (command, data) => chiaServer.sendCommand("chia_wallet", command, data);
-      replServer.context.farmer = async (command, data) => chiaServer.sendCommand("chia_farmer", command, data);
-      replServer.context.harvester = async (command, data) => chiaServer.sendCommand("chia_harvester", command, data);
-      replServer.context.crawler = async (command, data) => chiaServer.sendCommand("chia_crawler", command, data);
-    }
-  }
 });
 
 replServer.defineCommand('disconnect', {
-  help: 'Closes the websocket connection to the chia daemon.',
-  action() {
-    if (replServer.context.chiaServer !== undefined) {
-      replServer.context.chiaServer.disconnect();
-      replServer.context.chiaServer = undefined;
-      // clear all these out so they aren't avaialbe in the repl when not connected
-      replServer.context.daemon = undefined;
-      replServer.context.full_node = undefined;
-      replServer.context.wallet = undefined;
-      replServer.context.farmer = undefined;
-      replServer.context.harvester = undefined;
-      replServer.context.crawler = undefined;
-    } else {
-      console.log("not connected");
-      replServer.displayPrompt();
+    help: 'Closes the websocket connection to the chia daemon.',
+    action() {
+        if (replServer.context.chiaServer !== undefined) {
+            replServer.context.chiaServer.disconnect();
+            replServer.context.chiaServer = undefined;
+            // clear all these out so they aren't avaialbe in the repl when not connected
+            replServer.context.daemon = undefined;
+            replServer.context.full_node = undefined;
+            replServer.context.wallet = undefined;
+            replServer.context.farmer = undefined;
+            replServer.context.harvester = undefined;
+            replServer.context.crawler = undefined;
+        } else {
+            console.log('not connected');
+            replServer.displayPrompt();
+        }
     }
-  }
 });
