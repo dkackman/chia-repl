0c7d7eb669692e52182fc94e0188e7a94d3b5603
Don Kackman <dkackman@gmail.com>
2022-04-08 13:13:43 -0500

    i think that works

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import { start } from 'repl';
 import { Chia } from './chia.js';
 import { getSetting, saveSetting, defaultOptions } from './settings.js';
+import * as clvm_tools from 'clvm_tools';
 
 const replServer = start({ prompt: '> ', useColors: true });
+
+/* jshint ignore:start */
+await clvm_tools.initialize();
+/* jshint ignore:end */
+
+// this lifts the last clvm_tools from form a parameter 
+// to a retrn value so the repl can access it
+var last_clvm_result;
+clvm_tools.setPrintFunction((message) => last_clvm_result = message);
+
+function do_clvm(command, ...args) {
+    clvm_tools.go(command, ...args);
+
+    return last_clvm_result;
+}
+
 initializeContext();
 
 function initializeContext() {
     replServer.context.options = getSetting('.options', defaultOptions);
+    replServer.context.run = (...args) => do_clvm('run', ...args);
+    replServer.context.brun = (...args) => do_clvm('brun', ...args);
+    replServer.context.opd = (...args) => do_clvm('opd', ...args);
+    replServer.context.opc = (...args) => do_clvm('opc', ...args);            
+    replServer.context.read_ir = (...args) => do_clvm('read_ir', ...args);            
 }
 
 function connect() {
