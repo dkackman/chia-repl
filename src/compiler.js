0572d16087f2d37d0c8bf1965e5b7a58157ca92d
Don Kackman <dkackman@gmail.com>
2022-04-11 10:30:34 -0500

    lint

--- src/compiler.js
+++ src/compiler.js
@@ -1,6 +1,5 @@
 import * as _clvm_tools from 'clvm_tools';
 import utils from './chia-utils/chia-utils.js'; // temp fork unitl https://github.com/CMEONE/chia-utils/pull/7 is merged
-import * as fs from 'fs';
 
 /* jshint ignore:start */
 await _clvm_tools.initialize();
@@ -20,17 +19,11 @@ export function compile(chiaLisp, prefix, ...args) {
     const puzzle = do_clvm("opc", clvm);
     const address = utils.puzzle_hash_to_address(hash, prefix);
 
-    // if the source is a file, load the contents and attach to result
-    //if (fs.existsSync(chiaLisp)) {
-    //    chiaLisp = fs.readFileSync(chiaLisp).toString();
-    //}
-
     return {
         address: address,
         clvm: clvm,
         puzzle: puzzle,
         puzzle_hash: hash,
-        //source: chiaLisp,
     };
 }
 
