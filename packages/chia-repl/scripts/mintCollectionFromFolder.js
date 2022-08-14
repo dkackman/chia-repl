34ff62c28a4d42722a5e5709a73ff1b3f082b155
Don Kackman <dkackman@gmail.com>
2022-08-14 16:56:55 -0500

    commit-stash

--- packages/chia-repl/scripts/mintCollectionFromFolder.js
+++ packages/chia-repl/scripts/mintCollectionFromFolder.js
@@ -1,3 +1,35 @@
+
+async function mintNFT(walletId, targetAddress, fullFilePath, collectionMetaData, editionNumber, editionTotal, fee, royaltyAddress, royaltyPercentage) {
+    let dataFileInfo = {
+        name: path.getbasename(fullFilePath, path.getextension(fullFilePath)),
+        type: type,
+        filepath: fullFilePath
+    };
+
+    let mintingInfo = {
+        wallet_id: walletId,
+        target_address: targetAddress,
+        royalty_address: royaltyAddress,
+        royalty_percentage: royaltyPercentage,
+        fee: fee,
+        editionNumber: editionNumber,
+        editionTotal: editionTotal,
+    };
+
+    let nftMetadataStub = metadataFactory.createNftMetadata(dataFileInfo.name, collectionMetaData);
+    const metedataFileName = path.join(path.dirname(fillpath), getbasename(fullFilePath, path.getextension(fullFilePath), '.metadata.json'));
+    fs.readFile(metedataFileName, 'utf-8', file => {
+        metadata = JSON.parse(file);
+        nftMetadataStub = {
+            ...nftMetadataStub,
+            ...metadata
+        };
+    });
+
+    return await minter.createNftFromFile(dataFileInfo, mintingInfo, nftMetadataStub);
+}
+
+
 async function mintCollectionFromFolder(walletId, targetAddress, collectionMetaData, folderPath, nftFileExt, fee = 0, royaltyAddress = targetAddress, royaltyPercentage = 0) {
     // this is a link to an Apache 2.0 pdf
     minter.licenseFileInfo = { type: 'application/pdf', uri: 'https://bafybeibbnjrv3nwlq6gnx3bd5l2uwozxek4sm6ad4j2gazgv3paouldvai.ipfs.nftstorage.link/license' };
@@ -20,26 +52,4 @@ async function mintCollectionFromFolder(walletId, targetAddress, collectionMetaD
     });
 
     return spendBundles;
-}
-
-async function mintNFT(walletId, targetAddress, fullFilePath, collectionMetaData, editionNumber, editionTotal, fee, royaltyAddress, royaltyPercentage) {
-    let dataFileInfo = {
-        name: path.getbasename(fullFilePath, path.getextension(fullFilePath)),
-        type: type,
-        filepath: fullFilePath
-    };
-
-    let mintingInfo = {
-        wallet_id: walletId,
-        target_address: targetAddress,
-        royalty_address: royaltyAddress,
-        royalty_percentage: royaltyPercentage,
-        fee: fee,
-        editionNumber: editionNumber,
-        editionTotal: editionTotal,
-    };
-
-    let nftMetadata = metadataFactory.createNftMetadata(dataFileInfo.name);
-
-    await minter.createNftFromFile(dataFileInfo, mintingInfo, nftMetadata);
 }
