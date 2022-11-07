504fb1e642ee76a76f7bed0910f394f8e690e894
Don Kackman <dkackman@gmail.com>
2022-11-06 18:01:52 -0600

    comments

--- packages/chia-daemon/message_queue.js
+++ packages/chia-daemon/message_queue.js
@@ -11,6 +11,9 @@ export default class MessageQueue extends EventEmitter {
         this.wallet = wallet;
     }
 
+    /*
+        Peeks the next count messages from the queue. does not remove them
+    */
     async peekMessages(count = 1) {
         const payload = {
             start: 0,
@@ -20,22 +23,40 @@ export default class MessageQueue extends EventEmitter {
         return getResponse.notifications ?? [];
     }
 
+    /*
+        takes the next `count` messages and removes them from the queue
+    */
     async popMessages(count = 1) {
         const messages = await this.peekMessages(count);
         await this.deleteMessages(messages);
         return messages;
     }
 
-    async deleteMessages(messages) {
+    /*
+        deletes all of the supplied messages
+    */
+    async deleteMessages(messages = []) {
+        if (_.isArray(messages)) {
+            throw new Error('messages must be an array');
+        }
         const ids = _.map(messages, message => message.id);
         await this.wallet.delete_notifications(ids);
     }
 
+    /*
+        interupts any running listen loop
+    */
     stop() {
         this.stop = true;
     }
 
+    /*
+        polls the queue preidoically raising `message-received` event
+        for each message. Will continue to notify on messages until they are deleted
+    */
     async listen(messageCount = 1, pollSeconds = 10) {
+        this.stop = false;
+
         const timer = ms => new Promise(res => setTimeout(res, ms));
         while (this.stop !== true) {
             const messages = await this.peekMessages(messageCount);
