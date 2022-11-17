c2ff0094391340ed1c323164cda05f42bd138f71
Don Kackman <dkackman@gmail.com>
2022-11-16 19:49:52 -0600

    lint

--- packages/chia-bot-tests/messageQueue.ci.test.js
+++ packages/chia-bot-tests/messageQueue.ci.test.js
@@ -1,17 +1,7 @@
 import chai from 'chai';
 import { MessageQueue } from 'chia-bot';
-import { ChiaDaemon } from 'chia-daemon';
-import _ from 'lodash';
 
 const expect = chai.expect;
-
-const valid_connection = {
-    host: 'chiapas',
-    port: 55400,
-    key_path: '~/.chia/mainnet - chiapas/config/ssl/daemon/private_daemon.key',
-    cert_path: '~/.chia/mainnet - chiapas/config/ssl/daemon/private_daemon.crt',
-    timeout_seconds: 10,
-};
 
 describe('chia-bot', () => {
     describe('MessageQueue', () => {
