321fc1f08676fc81e5eea67c50838da9ca22325a
Don Kackman <dkackman@gmail.com>
2023-08-29 18:15:30 -0500

    spelling

--- packages/chia-daemon/chia_daemon.js
+++ packages/chia-daemon/chia_daemon.js
@@ -1,17 +1,17 @@
-import { randomBytes } from 'crypto';
-import { WebSocket } from 'ws';
-import { readFileSync } from 'fs';
-import createRpcProxy from './rpc_proxy.js';
-import { EventEmitter } from 'events';
-import untildify from './untildify.js';
-import _ from 'lodash';
+import { randomBytes } from "crypto";
+import { WebSocket } from "ws";
+import { readFileSync } from "fs";
+import createRpcProxy from "./rpc_proxy.js";
+import { EventEmitter } from "events";
+import untildify from "./untildify.js";
+import _ from "lodash";
 
 /** This can be found in the config but here for convenience. */
 export let localDaemonConnection = {
-    host: 'localhost',
+    host: "localhost",
     port: 55400,
-    key_path: '~/.chia/mainnet/config/ssl/daemon/private_daemon.key',
-    cert_path: '~/.chia/mainnet/config/ssl/daemon/private_daemon.crt',
+    key_path: "~/.chia/mainnet/config/ssl/daemon/private_daemon.key",
+    cert_path: "~/.chia/mainnet/config/ssl/daemon/private_daemon.crt",
     timeout_seconds: 30,
 };
 
@@ -28,13 +28,13 @@ export default class ChiaDaemon extends EventEmitter {
      * @param {number} connection.port - The daemon's listening port.
      * @param {string} connection.key_path - File path to the certificate key file used to secure the connection.
      * @param {string} connection.cert_path - File path to the certificate crt file used to secure the connection.
-     * @param {number} connection.timeout_seconds - Timeout, in seconds, for each call to the deamon.
+     * @param {number} connection.timeout_seconds - Timeout, in seconds, for each call to the daemon.
      * @param {string} service_name - the name of the client application or service talking to the daemon.
      */
-    constructor(connection, service_name = 'my_chia_app') {
+    constructor(connection, service_name = "my_chia_app") {
         super();
         if (connection === undefined) {
-            throw new Error('Connection meta data must be provided');
+            throw new Error("Connection meta data must be provided");
         }
 
         this.connection = connection;
@@ -49,7 +49,7 @@ export default class ChiaDaemon extends EventEmitter {
     }
 
     /**
-     * Property indicating whether the chia daemon websocket is currently conneted
+     * Property indicating whether the chia daemon websocket is currently connected
      */
     get connected() {
         return this.ws !== undefined;
@@ -62,14 +62,14 @@ export default class ChiaDaemon extends EventEmitter {
      */
     get services() {
         return {
-            daemon: createRpcProxy(this, 'daemon'),
-            full_node: createRpcProxy(this, 'chia_full_node'),
-            wallet: createRpcProxy(this, 'chia_wallet'),
-            farmer: createRpcProxy(this, 'chia_farmer'),
-            harvester: createRpcProxy(this, 'chia_harvester'),
-            crawler: createRpcProxy(this, 'chia_crawler'),
-            simulator: createRpcProxy(this, 'chia_full_node_simulator'),
-            data_layer: createRpcProxy(this, 'chia_data_layer'),
+            daemon: createRpcProxy(this, "daemon"),
+            full_node: createRpcProxy(this, "chia_full_node"),
+            wallet: createRpcProxy(this, "chia_wallet"),
+            farmer: createRpcProxy(this, "chia_farmer"),
+            harvester: createRpcProxy(this, "chia_harvester"),
+            crawler: createRpcProxy(this, "chia_crawler"),
+            simulator: createRpcProxy(this, "chia_full_node_simulator"),
+            data_layer: createRpcProxy(this, "chia_data_layer"),
         };
     }
 
@@ -84,11 +84,11 @@ export default class ChiaDaemon extends EventEmitter {
      */
     async connect() {
         if (this.connected) {
-            throw new Error('Already connected');
+            throw new Error("Already connected");
         }
 
         const address = `wss://${this.connection.host}:${this.connection.port}`;
-        this.emit('connecting', address);
+        this.emit("connecting", address);
 
         // the lifetime of the websocket is between connect and disconnect
         const ws = new WebSocket(address, {
@@ -97,45 +97,53 @@ export default class ChiaDaemon extends EventEmitter {
             cert: readFileSync(untildify(this.connection.cert_path)),
         });
 
-        ws.once('open', () => {
-            const msg = formatMessage('daemon', 'register_service', this._service_name, { service: this._service_name });
+        ws.once("open", () => {
+            const msg = formatMessage(
+                "daemon",
+                "register_service",
+                this._service_name,
+                { service: this._service_name }
+            );
             ws.send(JSON.stringify(msg));
         });
 
-        ws.once('close', () => {
-            this.emit('disconnected');
+        ws.once("close", () => {
+            this.emit("disconnected");
         });
 
-        ws.on('message', (data) => {
+        ws.on("message", (data) => {
             const msg = JSON.parse(data);
 
             if (this.outgoing.has(msg.request_id)) {
                 this.outgoing.delete(msg.request_id);
                 this.incoming.set(msg.request_id, msg);
-            } else if (msg.command === 'register_service') {
+            } else if (msg.command === "register_service") {
                 this.ws = ws;
-                this.emit('connected');// we consider ourselves connected only after register_service succeeds
+                this.emit("connected"); // we consider ourselves connected only after register_service succeeds
             } else {
                 // received a socket message that was not a response to something we sent
-                this.emit('event-message', msg);
+                this.emit("event-message", msg);
             }
         });
 
         let error = false;
-        ws.on('error', (e) => {
+        ws.on("error", (e) => {
             error = true;
-            this.emit('socket-error', e);
+            this.emit("socket-error", e);
         });
 
         const timeout_milliseconds = this.connection.timeout_seconds * 1000;
-        const timer = ms => new Promise(res => setTimeout(res, ms));
+        const timer = (ms) => new Promise((res) => setTimeout(res, ms));
         const start = Date.now();
 
         // wait here until connected goes to true, there is an error or we timeout
         while (!this.connected && !error) {
             await timer(100);
             if (Date.now() - start > timeout_milliseconds) {
-                this.emit('socket-error', new Error('Connection timeout expired'));
+                this.emit(
+                    "socket-error",
+                    new Error("Connection timeout expired")
+                );
                 break;
             }
         }
@@ -146,12 +154,12 @@ export default class ChiaDaemon extends EventEmitter {
     /** Closes the websocket and clears all state */
     disconnect() {
         if (!this.connected) {
-            throw new Error('Not connected');
+            throw new Error("Not connected");
         }
 
         this.ws.close();
-        this.ws.removeAllListeners('message');
-        this.ws.removeAllListeners('error');
+        this.ws.removeAllListeners("message");
+        this.ws.removeAllListeners("error");
         this.ws = undefined;
 
         this.incoming.clear();
@@ -167,20 +175,25 @@ export default class ChiaDaemon extends EventEmitter {
      */
     async sendCommand(destination, command, data = {}) {
         if (!this.connected) {
-            throw new Error('Not connected');
+            throw new Error("Not connected");
         }
 
-        const outgoingMsg = formatMessage(destination, command, this._service_name, data);
+        const outgoingMsg = formatMessage(
+            destination,
+            command,
+            this._service_name,
+            data
+        );
 
         this.outgoing.set(outgoingMsg.request_id, outgoingMsg);
         this.ws.send(JSON.stringify(outgoingMsg));
 
-        const timer = ms => new Promise(res => setTimeout(res, ms));
+        const timer = (ms) => new Promise((res) => setTimeout(res, ms));
         const start = Date.now();
         const timeoutMilliseconds = this.connection.timeout_seconds * 1000;
 
         // wait here until an incoming response shows up
-        // and because we're waiting asynchornously we might become disconencted in the meantime
+        // and because we're waiting asynchronously we might become disconnected in the meantime
         while (this.connected && !this.incoming.has(outgoingMsg.request_id)) {
             await timer(100);
             const elapsed = Date.now() - start;
@@ -192,7 +205,7 @@ export default class ChiaDaemon extends EventEmitter {
                 if (this.incoming.has(outgoingMsg.request_id)) {
                     this.incoming.delete(outgoingMsg.request_id);
                 }
-                throw new Error('Timeout expired');
+                throw new Error("Timeout expired");
             }
         }
 
@@ -217,7 +230,7 @@ function formatMessage(destination, command, origin, data = {}) {
         origin: origin,
         destination: destination,
         ack: false,
-        request_id: randomBytes(32).toString('hex'),
+        request_id: randomBytes(32).toString("hex"),
         data: data,
     };
 }
@@ -246,10 +259,10 @@ function formatMessage(destination, command, origin, data = {}) {
  */
 
 /**
- * socket-error event. Fires when the WebSocket reaises an error event.
+ * socket-error event. Fires when the WebSocket raises an error event.
  *
  * @event ChiaDaemon#socket-error
- * @property {Error} e - The Error object rasied from the WebSocket.
+ * @property {Error} e - The Error object raised from the WebSocket.
  */
 
 /**
