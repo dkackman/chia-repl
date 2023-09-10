f356897172201642d9ea51a3390d895eff6e6539
Don Kackman <dkackman@gmail.com>
2023-09-10 10:00:07 -0500

    loadUIConfig -> createChiaConnectionFromConfig

--- packages/chia-daemon/config.js
+++ packages/chia-daemon/config.js
@@ -1,9 +1,4 @@
-// adapted from https://github.com/Chia-Network/chia-blockchain-gui
-import yaml from "js-yaml";
-import fs from "fs";
-import path from "path";
-import _ from "lodash";
-import { getChiaRoot } from "chia-service-connector";
+import { createChiaConnectionFromConfig } from "chia-service-connector";
 
 /**
  * Loads the daemon connection details from the default config's ui section.
@@ -11,47 +6,5 @@ import { getChiaRoot } from "chia-service-connector";
  * @returns Connection details.
  */
 export default function loadUIConfig() {
-    const config = readConfigFile();
-
-    const selfHostname = _.get(config, "ui.daemon_host", "localhost");
-    const daemonPort = _.get(config, "ui.daemon_port", 55400);
-
-    const configRootDir = getChiaRoot();
-
-    const certPath = path.resolve(
-        configRootDir,
-        _.get(
-            config,
-            "ui.daemon_ssl.private_crt",
-            "config/ssl/daemon/private_daemon.crt"
-        )
-    );
-    const keyPath = path.resolve(
-        configRootDir,
-        _.get(
-            config,
-            "ui.daemon_ssl.private_key",
-            "config/ssl/daemon/private_daemon.key"
-        )
-    );
-
-    return {
-        service: "daemon",
-        host: selfHostname,
-        port: daemonPort,
-        key_path: keyPath,
-        cert_path: certPath,
-        timeout_seconds: 30,
-    };
-}
-
-function readConfigFile() {
-    const configRootDir = getChiaRoot();
-
-    return yaml.load(
-        fs.readFileSync(
-            path.resolve(configRootDir, "config/config.yaml"),
-            "utf8"
-        )
-    );
+    return createChiaConnectionFromConfig("ui");
 }
